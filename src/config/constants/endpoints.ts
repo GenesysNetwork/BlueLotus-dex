d8f51e33def5c7db784ae45d9ea0eae65fa1b254
Iv7dev <iv7.software@gmail.com>
2022-10-03 11:46:00 -0300

    thegraph fix apis

--- src/config/constants/endpoints.ts
+++ src/config/constants/endpoints.ts
@@ -1,5 +1,5 @@
 // export const GRAPH_API_PROFILE = process.env.NEXT_PUBLIC_GRAPH_API_PROFILE
-// export const GRAPH_API_PREDICTION = process.env.NEXT_PUBLIC_GRAPH_API_PREDICTION
+export const GRAPH_API_PREDICTION = process.env.NEXT_PUBLIC_GRAPH_API_PREDICTION
 // export const GRAPH_API_LOTTERY = process.env.NEXT_PUBLIC_GRAPH_API_LOTTERY
 // export const SNAPSHOT_BASE_URL = process.env.NEXT_PUBLIC_SNAPSHOT_BASE_URL
 // export const API_PROFILE = process.env.NEXT_PUBLIC_API_PROFILE
@@ -21,7 +21,7 @@
 // export const TC_MOBOX_SUBGRAPH = 'https://api.thegraph.com/subgraphs/name/pancakeswap/trading-competition-v3'
 // export const TC_MOD_SUBGRAPH = 'https://api.thegraph.com/subgraphs/name/pancakeswap/trading-competition-v4'
 
-// export const GALAXY_NFT_CLAIMING_API = 'https://graphigo.prd.galaxy.eco/query'
+export const GALAXY_NFT_CLAIMING_API = 'https://graphigo.prd.galaxy.eco/query'
 
 /**
  * V1 will be deprecated but is still used to claim old rounds
